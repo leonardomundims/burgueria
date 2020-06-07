df592d4488e8196ffc94e1827b0e0b9a0dc006f0
Leonardo Mundim <leo.mundimsouza@gmail.com>
2020-06-07 18:25:02 -0300

    visual console

--- pedidosConsole/Program.cs
+++ pedidosConsole/Program.cs
@@ -4,33 +4,51 @@ using System.Collections.Generic;
 
 namespace pedidosConsole {
     class Program {
+
         static void Main(string[] args) {
-            var item = new ItemPedido();
-
-            Console.WriteLine("---- Pedido -----");
-            Console.WriteLine("Gostaria de adicionar um item ao pedido? s/n");
-            var decisao = Console.ReadLine();
+            var pedido = new List<ItemPedido>();
+            string decisao;
 
 
-            if(decisao == "s") {
-                //pegar a descrição do item
+            Console.WriteLine("---- Faça o pedido -----");
+            
+
+
+            do {
+                var item = new ItemPedido();
+                //pegar a descrição do item[i]
                 Console.WriteLine("Descreva o item");
                 item.Descricao = Console.ReadLine();
 
-                //pegar o valor unitario do item
+                //pegar o valor unitario do item[i]
                 Console.WriteLine("Digite o valor do item");
                 double.TryParse(Console.ReadLine(), out double valorUnitario);
                 item.ValorUnitario = valorUnitario;
-                
-                //pegar a quantidade do item
+
+                //pegar a quantidade do item[i]
                 Console.WriteLine("Digite a quantidade do item");
                 int.TryParse(Console.ReadLine(), out int quantidade);
                 item.Quantidade = quantidade;
 
-                item.Imprimir(item);
-            } else {
-                Environment.Exit(0);
-            }
+                // adicionar o item ao pedido
+                pedido.Add(item);
+
+                //exibir o pedido
+                Console.WriteLine("------- PEDIDO -------");
+                Console.WriteLine("# - ITEM - VALOR UNITARIO - QTD");
+                int i = 1;
+                foreach (var elemento in pedido) {
+                    
+                    Console.WriteLine($"{i} - {elemento.Descricao} - {elemento.ValorUnitario} - {elemento.Quantidade}");
+                    Console.WriteLine("-------------------");
+                    i++;
+                }
+                
+                Console.WriteLine("Gostaria de adicionar um item ao pedido? s/n");
+                decisao = Console.ReadLine();
+
+
+            } while (decisao == "s");
 
             
         }
