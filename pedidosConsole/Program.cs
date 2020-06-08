6fa47334319026adbc29f0bf17db0ba6ee4e46e8
Leonardo Mundim <leo.mundimsouza@gmail.com>
2020-06-07 23:19:16 -0300

    multiplos pedidos

--- pedidosConsole/Program.cs
+++ pedidosConsole/Program.cs
@@ -7,47 +7,59 @@ namespace pedidosConsole {
     class Program {
 
         static void Main(string[] args) {
-            var pedido = new Pedido();
-            string decisao;
+            string decidir;
+            int i = 1;
+
+            Console.WriteLine("Gostaria de fazer um pedido? s/n");
+            decidir = Console.ReadLine();
+
+            while(decidir == "s") {
+                var pedido = new Pedido();
+                string decisao;
+
+                Console.WriteLine("---- Faça o pedido -----");
+
+                do {
+                    var item = new ItemPedido();
+                    //pegar a descrição do item[i]
+                    Console.WriteLine("Descreva o item");
+                    item.Descricao = Console.ReadLine();
+
+                    //pegar o valor unitario do item[i]
+                    Console.WriteLine("Digite o valor do item");
+                    double.TryParse(Console.ReadLine(), out double valorUnitario);
+                    item.ValorUnitario = valorUnitario;
+
+                    //pegar a quantidade do item[i]
+                    Console.WriteLine("Digite a quantidade do item");
+                    int.TryParse(Console.ReadLine(), out int quantidade);
+                    item.Quantidade = quantidade;
+
+                    // adicionar o item ao pedido
+                    pedido.Adicionar(item);
+
+                    //exibir o pedido
+                    pedido.Imprimir();
+
+                    Console.WriteLine("Gostaria de adicionar um item ao pedido? s/n");
+                    decisao = Console.ReadLine();
 
 
-            Console.WriteLine("---- Faça o pedido -----");
-            
+                } while (decisao == "s");
+
+                var total = pedido.Total();
+                Console.WriteLine("--------------");
+                Console.WriteLine($"Total do pedido {i}: " + total.ToString("C2", CultureInfo.CurrentCulture));
+                i++;
+
+                Console.WriteLine("Gostaria de fazer um novo pedido? s/n ");
+                decidir = Console.ReadLine();
+
+            }
 
 
-            do {
-                var item = new ItemPedido();
-                //pegar a descrição do item[i]
-                Console.WriteLine("Descreva o item");
-                item.Descricao = Console.ReadLine();
-
-                //pegar o valor unitario do item[i]
-                Console.WriteLine("Digite o valor do item");
-                double.TryParse(Console.ReadLine(), out double valorUnitario);
-                item.ValorUnitario = valorUnitario;
-
-                //pegar a quantidade do item[i]
-                Console.WriteLine("Digite a quantidade do item");
-                int.TryParse(Console.ReadLine(), out int quantidade);
-                item.Quantidade = quantidade;
-
-                // adicionar o item ao pedido
-                pedido.Adicionar(item);
-
-                //exibir o pedido
-                pedido.Imprimir();
-                
-                Console.WriteLine("Gostaria de adicionar um item ao pedido? s/n");
-                decisao = Console.ReadLine();
-
-
-            } while (decisao == "s");
-
-            var total = pedido.Total();
-            Console.WriteLine("--------------");
-            Console.WriteLine("Total do pedido: " + total.ToString("C2", CultureInfo.CurrentCulture));
-
-
+            Console.WriteLine("Obrigado por utilizar o programa");
+ 
         }
     }
 }
