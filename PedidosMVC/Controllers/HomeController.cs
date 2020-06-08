0aa89face1c8a2d5bde892ea0f8f4c8debf17ab1
Leonardo Mundim <leo.mundimsouza@gmail.com>
2020-06-08 00:07:49 -0300

    add projeto mvc

---
+++ PedidosMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedidosMVC.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+
+
+        //pagina de pedidos
+        public IActionResult Pedido() {
+            return View();
+        }
+
+        public IActionResult Pedidos() {
+            return View();
+        }
+    }
+}
